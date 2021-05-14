88ff8e3c1eadfe75e42f32b4b4d27d7d46d48139
nbekenov <nur.beken.usa@gmail.com>
2021-05-14 16:36:29 -0400

    add method inside class

--- api-example/main.py
+++ api-example/main.py
@@ -21,9 +21,8 @@ from imports.aws import (
 
 # constants
 tf_bucket_name = os.environ["TF_STATE_BUCKET_NAME"]
+stack_name = "api-example"
 
-class CreateRestApi():
-    pass
 
 class MyApiStack(TerraformStack):
     """
@@ -33,22 +32,20 @@ class MyApiStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
+    def create_api(self, api_name, api_spec_yaml, stage):
+
         # define resources here
         tags = {"ManagedBy": "terraform CDK", "user": "good.gentleman"}
 
-        
-        spec_yaml = "openapi-test.yaml"
-        with open(spec_yaml, 'r') as file:
+        with open(api_spec_yaml, "r") as file:
             data = yaml.load(file)
             open_api_body = yaml.dump(data)
-
-        AwsProvider(self, "Aws", region="us-east-1")
 
         # aws_api_gateway_rest_api
         rest_api = ApiGatewayRestApi(
             self,
-            "my_api",
-            name="paymentconfig-api-test",
+            f"api-{api_name}",
+            name=api_name,
             endpoint_configuration=[
                 ApiGatewayRestApiEndpointConfiguration(types=["REGIONAL"])
             ],
@@ -57,23 +54,25 @@ class MyApiStack(TerraformStack):
 
         # aws_api_gateway_deployment
         api_deployment = ApiGatewayDeployment(
-            self, "my_api_deployment", rest_api_id=rest_api.id
+            self, f"deployment-{api_name}", rest_api_id=rest_api.id
         )
 
         # aws_api_gateway_stage
         api_stage = ApiGatewayStage(
             self,
-            "my_api_stage",
+            f"stage-{api_name}",
             deployment_id=api_deployment.id,
             rest_api_id=rest_api.id,
-            stage_name="dev",
+            stage_name=stage,
         )
 
         # aws_api_gateway_method_settings
-        loging_settings = ApiGatewayMethodSettingsSettings(logging_level="ERROR", metrics_enabled=True)
+        loging_settings = ApiGatewayMethodSettingsSettings(
+            logging_level="ERROR", metrics_enabled=True
+        )
         method_settings = ApiGatewayMethodSettings(
             self,
-            "all",
+            f"method_setting-{api_name}",
             rest_api_id=rest_api.id,
             stage_name=api_stage.stage_name,
             method_path="*/*",
@@ -86,7 +85,15 @@ def main():
     build resources via CDK-TF
     """
     app = App()
-    stack = MyApiStack(app, "api-example")
+    stack = MyApiStack(app, stack_name)
+
+    stack.create_api(
+        api_name="paymentconfig", api_spec_yaml="openapi-test.yaml", stage="dev"
+    )
+    stack.create_api(
+        api_name="paymentconfig-mtls", api_spec_yaml="openapi-mtls.yaml", stage="dev"
+    )
+
     # configure TF backend to use S3 to store state file
     stack.add_override(
         "terraform.backend",
