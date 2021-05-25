048afc8e1c14b1b1137ed076af65feb4acb5f2d6
nbekenov <nur.beken.usa@gmail.com>
2021-05-25 16:46:03 -0400

    update methods

--- lambda-example/main.py
+++ lambda-example/main.py
@@ -9,19 +9,15 @@ from pathlib import Path
 
 from cdktf import App, TerraformStack
 from constructs import Construct
+from imports.aws import AwsProvider
 
-# from imports.aws import AwsProvider
-
-# from imports.terraform_aws_modules.lambda.aws import
-# TerraformAwsModulesLambdaAws will fail cause path contains key word Import a
-# module from a path that contains a reserved Python keyword like
 try:
     mymodule = importlib.import_module("imports.terraform_aws_modules.lambda.aws")
 except ModuleNotFoundError as err:
     sys.exit(f'{err}: module not found, need to run "cdktf get"')
 
 # constants
-# region = os.environ["AWS_DEFAULT_REGION"]
+region = os.environ["AWS_DEFAULT_REGION"]
 tf_bucket_name = os.environ["TF_STATE_BUCKET_NAME"]
 
 
@@ -34,25 +30,32 @@ class MyLambdaStack(TerraformStack):
         super().__init__(scope, ns)
 
         # define resources here
-        # AwsProvider(self, "Aws", region=region)
 
-        tags = {"ManagedBy": "terraform CDK", "user": "good.gentlemen"}
+        self.tags = {"ManagedBy": "terraform CDK", "user": "good.gentlemen"}
+
+    def create_lambda(self, service_conf):
+
+        service_name = service_conf["service_name"]
+        handler = service_conf["handler"]
 
         source_path = Path(__file__).parent / "src/handlers"
         if not source_path.is_dir():
             sys.exit(f"Handlers missing -- {source_path} not a directory")
-
+            
+        print(str(source_path))
         mymodule.TerraformAwsModulesLambdaAws(
             self,
-            "my_lambda_function",
+            f"{service_name}-lambda_function",
             source_path=str(source_path),
-            function_name="my-test-lambda",
-            description="Simple hello world deployed by Terraform",
-            handler="app.lambda_handler",
+            function_name=service_name,
+            handler=handler,
             runtime="python3.8",
             create_role=True,
             publish=True,
-            tags=tags,
+            memory_size = 512,
+            timeout = 30,
+            tags=self.tags,
+            environment_variables = {"ENV_VAR1": "terraform CDK", "ENV_VAR2": "good.gentlemen"}
         )
 
 
@@ -62,6 +65,21 @@ def main():
     """
     app = App()
     stack = MyLambdaStack(app, "lambda-example")
+
+
+    lambda_list = [
+        {
+            "service_name": "test-1",
+            "handler" : "app.lambda_handler"
+        },
+        {
+            "service_name": "test-2",
+            "handler" : "app.lambda_handler"
+        }        
+    ]
+
+    for item in lambda_list:
+        stack.create_lambda(item)
     # configure TF backend to use S3 to store state file
     stack.add_override(
         "terraform.backend",
@@ -69,7 +87,7 @@ def main():
             "s3": {
                 "bucket": tf_bucket_name,
                 "key": "terraform-state/lambda",
-                # "region": region,
+                "region": region,
                 "encrypt": True,
             }
         },
