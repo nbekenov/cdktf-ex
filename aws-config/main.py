8440fc2b48c532379d51cd7eb2373b98d66fb630
nbekenov <nur.beken.usa@gmail.com>
2021-08-13 10:36:45 -0400

    fix linter and add readme

--- aws-config/main.py
+++ aws-config/main.py
@@ -7,12 +7,13 @@ from constructs import Construct
 from cdktf import App, TerraformStack
 from boto3.session import Session
 from imports.aws import (
-    AwsProvider, IamRole,
+    AwsProvider,
+    IamRole,
     ConfigConfigurationRecorder,
     ConfigConfigurationRecorderRecordingGroup,
     ConfigDeliveryChannel,
     ConfigDeliveryChannelSnapshotDeliveryProperties,
-    ConfigConfigurationRecorderStatus
+    ConfigConfigurationRecorderStatus,
 )
 
 
@@ -23,12 +24,14 @@ class AWSConfig(TerraformStack):
         * enable_awsconfig_in_region
         * enable_awsconfig_in_account
         * add_config_rule - TBD
+        * TBD - support SNS topic
     """
+
     def __init__(self, scope: Construct, ns: str, profile_name, partition):
         super().__init__(scope, ns)
 
         # const
-        self.tags = { "project": "aws-waf-manager"}
+        self.tags = {"project": "aws-waf-manager"}
         self.environment = "dev".lower()
         self.application = "application".lower()
         self.resource_prefix = f"{self.application}-{self.environment}"
@@ -36,7 +39,8 @@ class AWSConfig(TerraformStack):
         self.profile_name = profile_name
         self.partition = partition
 
-
+    # Disable too-many-arguments
+    #pylint: disable=R0913
     def enable_awsconfig_in_region(
         self,
         region,
@@ -45,58 +49,77 @@ class AWSConfig(TerraformStack):
         create_role=True,
         iam_role_arn=None,
         topic_arn=None,
-        frequency="Six_Hours"
+        frequency="Six_Hours",
     ):
         """
         Enables AWS config in specified region for a particular account
         """
-        aws_provider = AwsProvider(self, f"aws-{region}", region=region, profile=self.profile_name, alias = f"aws-{region}")
+        aws_provider = AwsProvider(
+            self,
+            f"aws-{region}",
+            region=region,
+            profile=self.profile_name,
+            alias=f"aws-{region}",
+        )
         if create_role:
             iam_role_arn = self.__create_iam_role()
         print(f"enabling AWS Config for region = {region} ...")
 
-        recorder = ConfigConfigurationRecorder(self,f"recorder-{region}",
-            role_arn = iam_role_arn,
-            recording_group = [
-                ConfigConfigurationRecorderRecordingGroup(all_supported=True,
-                    include_global_resource_types=include_global_resources
+        recorder = ConfigConfigurationRecorder(
+            self,
+            f"recorder-{region}",
+            role_arn=iam_role_arn,
+            recording_group=[
+                ConfigConfigurationRecorderRecordingGroup(
+                    all_supported=True,
+                    include_global_resource_types=include_global_resources,
                 )
             ],
-            provider = aws_provider
+            provider=aws_provider,
         )
-        delivery_channel = ConfigDeliveryChannel(self, f"delivery_channel-{region}",
-            s3_bucket_name = bucket_name,
-            snapshot_delivery_properties = [
-                ConfigDeliveryChannelSnapshotDeliveryProperties(delivery_frequency=frequency)
+        delivery_channel = ConfigDeliveryChannel(
+            self,
+            f"delivery_channel-{region}",
+            s3_bucket_name=bucket_name,
+            snapshot_delivery_properties=[
+                ConfigDeliveryChannelSnapshotDeliveryProperties(
+                    delivery_frequency=frequency
+                )
             ],
-            sns_topic_arn = topic_arn,
-            depends_on = [recorder],
-            provider = aws_provider
+            sns_topic_arn=topic_arn,
+            depends_on=[recorder],
+            provider=aws_provider,
         )
-        recorder_status = ConfigConfigurationRecorderStatus(self, f"recorder_status-{region}",
-            is_enabled = True,
-            name = recorder.name,
-            depends_on = [delivery_channel],
-            provider = aws_provider
+        recorder_status = ConfigConfigurationRecorderStatus(
+            self,
+            f"recorder_status-{region}",
+            is_enabled=True,
+            name=recorder.name,
+            depends_on=[delivery_channel],
+            provider=aws_provider,
         )
+        recorder_status.override_logical_id(f"recorder_status-{region}")
 
     def enable_awsconfig_in_account(self, account_id, bucket_name):
         """
         Enables AWS config in all supported regions
         """
-        # This ensures that you don’t get redundant copies of IAM configuration Items in every Region.
-        record_global_resources_region = "us-gov-west-1" if "gov" in self.partition else "us-east-1"
+        # This ensures that you don’t get redundant
+        # copies of IAM configuration Items in every Region.
+        record_global_resources_region = (
+            "us-gov-west-1" if "gov" in self.partition else "us-east-1"
+        )
         iam_role_arn = self.__create_iam_role()
 
         for region in self.get_available_regions():
             self.enable_awsconfig_in_region(
-                region = region,
+                region=region,
                 bucket_name=bucket_name,
                 include_global_resources=region in record_global_resources_region,
-                create_role = False,
+                create_role=False,
                 iam_role_arn=iam_role_arn,
             )
-        print(f"AWS Config is enabled in all supported regions for account_id {account_id} in partition {self.partition}")
+        print(f"AWS Config is enabled in all regions for account_id {account_id}")
 
     def __create_iam_role(self):
         """
@@ -110,20 +133,22 @@ class AWSConfig(TerraformStack):
             "Statement": [
                 {
                     "Effect": "Allow",
-                    "Principal": {
-                        "Service": ["config.amazonaws.com"]
-                    },
-                    "Action": ["sts:AssumeRole"]
+                    "Principal": {"Service": ["config.amazonaws.com"]},
+                    "Action": ["sts:AssumeRole"],
                 }
-            ]
+            ],
         }
-        managed_policy_arn = f"arn:{self.partition}:iam::aws:policy/service-role/AWS_ConfigRole"
+        managed_policy_arn = (
+            f"arn:{self.partition}:iam::aws:policy/service-role/AWS_ConfigRole"
+        )
 
-        aws_config_role = IamRole(self,"aws_config_role",
-            name = f"{self.resource_prefix}-global-awsconfig-role",
-            path = "/",
-            assume_role_policy = json.dumps(assume_role_policy),
-            managed_policy_arns = [managed_policy_arn],
+        aws_config_role = IamRole(
+            self,
+            "aws_config_role",
+            name=f"{self.resource_prefix}-global-awsconfig-role",
+            path="/",
+            assume_role_policy=json.dumps(assume_role_policy),
+            managed_policy_arns=[managed_policy_arn],
         )
         print(f"IAM role for AWS Config was creted in region = {region} ")
         return aws_config_role.arn
@@ -134,7 +159,9 @@ class AWSConfig(TerraformStack):
         based on provided partition.
         """
         sess = Session()
-        aws_config_regions = sess.get_available_regions('config',partition_name=self.partition, allow_non_regional=False)
+        aws_config_regions = sess.get_available_regions(
+            "config", partition_name=self.partition, allow_non_regional=False
+        )
         return aws_config_regions
 
 
@@ -151,8 +178,12 @@ def main():
 
     for account, profile in accounts_list.items():
         app = App(stack_traces=False)
-        aws_config = AWSConfig(app, f"aws-config-{account}", profile_name=profile, partition=partition_name)
-        aws_config.enable_awsconfig_in_account(account_id=account, bucket_name=s3_bucket_name)
+        aws_config = AWSConfig(
+            app, f"aws-config-{account}", profile_name=profile, partition=partition_name
+        )
+        aws_config.enable_awsconfig_in_account(
+            account_id=account, bucket_name=s3_bucket_name
+        )
         app.synth()
 
 
